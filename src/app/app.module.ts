6ef131ba1a69c1fc19e943eb40211b44fcb5ae5c
ugrasenanv <ugrasenanv@gmail.com>
2022-03-09 01:12:18 +0530

    4 files modified, 14 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,36 @@
-import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
-
+import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavigationBarComponent } from './components/navigation-bar/navigation-bar.component';
+import { CreateCustomerComponent } from './components/create-customer/create-customer.component';
+import { UpdateCustomerComponent } from './components/update-customer/update-customer.component';
+import { ViewCustomersComponent } from './components/view-customers/view-customers.component';
+// import { ToastrModule } from "ng6-toastr-notifications";
+import { GetAllCustomersService } from './services/get-all-customers.service';
+import { SharedService } from './services/shared.service';
+import { GetACustomerService } from './services/get-acustomer.service';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    // ToastrModule.forRoot()
+  ],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    NavigationBarComponent,
+    CreateCustomerComponent,
+    UpdateCustomerComponent,
+    ViewCustomersComponent,
+  ],
+  bootstrap: [AppComponent],
+  providers: [GetAllCustomersService, SharedService, GetACustomerService],
 })
-export class AppModule { }
+export class AppModule {}
