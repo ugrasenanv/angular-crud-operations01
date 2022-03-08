a4b04ae7f1410486de0e7d964cb1d87ae583fcfc
ugrasenanv <ugrasenanv@gmail.com>
2022-03-09 02:22:53 +0530

    5 files modified, 16 added

---
+++ src/app/services/get-all-customers.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { Customer } from '../customer';
+import { customerList } from '../data/data';
+// import { Customer } from "../model/Customer";
+
+@Injectable()
+export class GetAllCustomersService {
+  customerList: Customer[] = customerList;
+  constructor() {}
+
+  getAllCustomers() {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        const data = { res: this.customerList };
+        resolve(data);
+      }, 2000);
+    });
+  }
+}
