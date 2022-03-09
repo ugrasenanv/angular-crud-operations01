8bd25f37c5c4354fef2f76f29cdea68fa8286906
ugrasenanv <ugrasenanv@gmail.com>
2022-03-09 23:23:27 +0530

    2 files modified

--- src/app/components/modal/modal.component.ts
+++ src/app/components/modal/modal.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { SharedTaskService } from 'src/app/task-services/shared.service';
 
 @Component({
   selector: 'app-modal',
@@ -10,38 +8,11 @@ import { SharedTaskService } from 'src/app/task-services/shared.service';
 export class ModalComponent {
   @ViewChild('myModal', { static: false }) modal: ElementRef;
 
-  createTaskForm: FormGroup;
-  constructor(
-    private formBuilder: FormBuilder,
-    private sharedService: SharedTaskService
-  ) {
-    sharedService.setViewTaskId(0);
-    this.createTaskForm = formBuilder.group({
-      id: [0],
-      name: ['', Validators.required],
-      project: ['', Validators.required],
-      Comments: ['', Validators.required],
-    });
-  }
-
-  // ngOnInit() {}
-
   open() {
     this.modal.nativeElement.style.display = 'block';
   }
 
   close() {
     this.modal.nativeElement.style.display = 'none';
-  }
-
-  createTask() {
-    console.log('create button clicked');
-    console.log('form value ' + JSON.stringify(this.createTaskForm.value));
-
-    if (this.createTaskForm.valid) {
-      // this.toastr.successToastr("This is a vaild form.", "Success!");
-    } else {
-      // this.toastr.warningToastr("This is not a valid form.", "Alert!");
-    }
   }
 }
