76e90813c97a03a8140411154cebc0f766adb949
ugrasenanv <ugrasenanv@gmail.com>
2022-03-09 08:45:13 +0530

    1 file modified

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,33 +1,33 @@
-import { ViewCustomersComponent } from "./components/view-customers/view-customers.component";
-import { NgModule } from "@angular/core";
-import { Routes, RouterModule } from "@angular/router";
-import { CreateCustomerComponent } from "./components/create-customer/create-customer.component";
-import { UpdateCustomerComponent } from "./components/update-customer/update-customer.component";
-import { HomeComponent } from "./components/home/home.component";
-import { ViewCustomerRouteGuardService } from "./routeGuards/view-customer-route-guard";
+import { ViewCustomersComponent } from './components/view-customers/view-customers.component';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { CreateCustomerComponent } from './components/create-customer/create-customer.component';
+import { UpdateCustomerComponent } from './components/update-customer/update-customer.component';
+import { HomeComponent } from './components/home/home.component';
+import { ViewCustomerRouteGuardService } from './routeGuards/view-customer-route-guard';
 
 const routes: Routes = [
   {
-    path: "create",
-    component: CreateCustomerComponent
+    path: 'create',
+    component: CreateCustomerComponent,
   },
   {
-    path: "update",
+    path: 'update',
     component: UpdateCustomerComponent,
-    canActivate: [ViewCustomerRouteGuardService]
+    canActivate: [ViewCustomerRouteGuardService],
   },
   {
-    path: "view",
-    component: ViewCustomersComponent
+    path: 'view',
+    component: ViewCustomersComponent,
   },
   {
-    path: "",
-    component: HomeComponent
-  }
+    path: '',
+    component: ViewCustomersComponent,
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class AppRoutingModule {}
